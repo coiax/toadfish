6e9204dcdc339e1f228de614f72e62e82732d0cf
Jack Edge <yellowbounder@gmail.com>
2017-09-15 16:52:02 +0100

    Got it working

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,12 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-screeps');
 
+    var my_options = require("./screeps-options.json")
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         screeps: {
-            options: require("./screeps-options"),
+            options: my_options,
             dist: {
                 src: ['dist/*.js']
             }
