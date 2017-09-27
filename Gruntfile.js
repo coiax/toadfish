78d967d8f0436f2768c13aa7d46e30fd4aaabc59
Jack Edge <yellowbounder@gmail.com>
2017-09-27 14:37:41 +0100

    Grunt cleans and stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    var my_options = JSON.parse(fs.readFileSync('./screeps-options', 'utf8'))
+    var my_options = JSON.parse(fs.readFileSync('./screeps-options', 'utf8'));
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -13,6 +15,29 @@ module.exports = function(grunt) {
             dist: {
                 src: ['dist/*.js']
             }
+        },
+        clean: {
+            'dist': ['dist/*.js']
+        },
+        // Copy all source files into the dist folder
+		// flattening the folder structure by converting
+		// path delimiters to underscores
+        copy: {
+          screeps: {
+            files: [{
+              expand: true,
+              cwd: 'src/',
+              src: '**',
+              dest: 'dist/',
+              filter: 'isFile',
+              rename: function (dest, src) {
+                return dest + src.replace(/\//g,'_');
+              }
+            }],
+          }
         }
     });
+
+	// Sets default `grunt` invocation to running these three tasks
+	grunt.registerTask('default',  ['clean', 'copy:screeps', 'screeps']);
 }
