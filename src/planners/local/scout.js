7997bc28fc7f5e0017548a54735a5433d57ebed5
Jack Edge <yellowbounder@gmail.com>
2017-10-26 17:31:19 +0100

    Scout planner

---
+++ src/planners/local/scout.js
@@ -0,0 +1,32 @@
+module.exports.run = function(room) {
+    // Store information for future consideration about expansion/attack
+    room.memory.scout = {};
+    var scout = room.memory.scout;
+
+    scout.time = Game.time;
+
+    if(room.controller) {
+        scout.controller = {
+            pos: room.controller.pos,
+            id: room.controller.id,
+            level: room.controller.level,
+            owner: room.controller.owner
+        }
+    }
+
+    var sources = room.find(FIND_SOURCES);
+    for(var i in sources) {
+        if(!scout.sources)
+            scout.sources = [];
+
+        var source = sources[i];
+        var item = {
+            id: source.id,
+            pos: source.pos,
+            energyCapacity: source.energyCapacity
+        };
+
+        scout.sources.push(item);
+    };
+
+}
--- src/planners/master.js
+++ src/planners/master.js
@@ -6,10 +6,11 @@ var construction = require("planners_local_construction");
 var towers = require("planners_local_towers");
 var family_planner = require("planners_local_family_planner");
 var ant_trail = require("planners_local_ant_trail");
+var scout = require("planners_local_scout");
 
 // Selected planners to run
 var global_planners = [room_counter];
-var local_planners = [construction, towers, family_planner];
+var local_planners = [construction, towers, family_planner, scout];
 
 module.exports.run = function() {
     for(var i in global_planners)
